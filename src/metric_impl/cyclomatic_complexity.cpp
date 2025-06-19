7e9e6f2a8254ee13fb774a2ea3711c92b9985f8b
igor.fadeev <igor.fadeev@lgepartner.com>
2025-06-19 12:22:11 +0000

    Add cyclomatic_complexity impl

--- src/metric_impl/cyclomatic_complexity.cpp
+++ src/metric_impl/cyclomatic_complexity.cpp
@@ -18,6 +18,15 @@
 
 namespace analyser::metric::metric_impl {
 
-// здесь ваш код
+MetricResult::ValueType CyclomaticComplexityMetric::CalculateImpl(const function::Function &f) const {
+
+    auto counter = std::ranges::count_if(f.ast | std::views::split(' '), [](auto &&elem) {
+        std::string st(elem.begin(), elem.end());
+        return (st.contains("if") || st.contains("elif") || st.contains("while") || st.contains("for") ||
+                st.contains("try") || st.contains("catch") || st.contains("finally") || st.contains("match") ||
+                st.contains("case") || st.contains("assert"));
+    });
+}
+std::string CyclomaticComplexityMetric::Name() const { return "CyclomaticComplexityMetric"; }
 
 }  // namespace analyser::metric::metric_impl
