e8a90c84ec445c871b62a5109ac8080016294c58
igor.fadeev <igor.fadeev@lgepartner.com>
2025-06-20 11:02:17 +0000

    Add Parameters counter

--- src/metric_impl/parameters_count.cpp
+++ src/metric_impl/parameters_count.cpp
@@ -9,6 +9,7 @@
 #include <functional>
 #include <iostream>
 #include <ranges>
+#include <set>
 #include <sstream>
 #include <string>
 #include <variant>
@@ -16,6 +17,29 @@
 
 namespace analyser::metric::metric_impl {
 
-// здесь ваш код
+MetricResult::ValueType CountParametersMetric::CalculateImpl(const function::Function &f) const {
+    extern std::optional<int> extract_number(const std::string &line);
+
+    int func_param_counter = 0;
+    int func_definition_line = -1;
+
+    for (auto el : f.ast | std::views::split('\n')) {
+        std::string substr(el.begin(), el.end());
+        auto opt_val = extract_number(substr);
+        if (opt_val.has_value()) {  // function definition placed on the 0-th line
+            if (substr.contains("function_definition")) {
+                func_definition_line = opt_val.value();
+            } else if (substr.contains("identifier") && opt_val.value() == func_definition_line) {
+                func_param_counter++;
+            }
+        }
+    }
+
+    func_param_counter--;  // to exclude "name: (identifier ...)" line
+
+    return func_param_counter;
+}
+
+std::string CountParametersMetric::Name() const { return "CountParametersMetric"; }
 
 }  // namespace analyser::metric::metric_impl
