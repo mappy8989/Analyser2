6a6519f0ca17ddbb431bca9cf416b97d5ef09331
IgorF <kleef@mail.ru>
2025-06-21 10:03:26 +0300

    Fix CountParametersMetric

--- src/metric_impl/parameters_count.cpp
+++ src/metric_impl/parameters_count.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <functional>
 #include <iostream>
+#include <print>
 #include <ranges>
 #include <set>
 #include <sstream>
@@ -22,17 +23,25 @@ MetricResult::ValueType CountParametersMetric::CalculateImpl(const function::Fun
     extern std::optional<int> extract_number(const std::string &line);
 
     int func_param_counter = 0;
-    int func_definition_line = -1;
 
     auto filtered_lines = f.ast | std::views::split('\n') |
                           std::views::transform([](const auto &&el) { return std::string(el.begin(), el.end()); }) |
                           std::views::filter([](const auto &str) { return extract_number(str).has_value(); });
 
     std::ranges::for_each(filtered_lines, [&](const auto &str) {
+        static int func_definition_line = -1;
+        static int parameters_spaces_count = 0;  // spaces count for function parameters line
         int line_num = extract_number(str).value();
+
+        int spaces_count = std::distance(str.begin(), std::ranges::find_if(str, [](char c) { return c != ' '; }));
         if (str.contains("function_definition")) {
             func_definition_line = line_num;
-        } else if (str.contains("identifier") && line_num == func_definition_line) {
+            parameters_spaces_count = 0;
+        } else if (str.contains("parameters") && line_num == func_definition_line && !parameters_spaces_count) {
+            parameters_spaces_count = spaces_count + 2;  // we have 2 more spaces after parameters definition
+        } else if ((str.contains("identifier") || str.contains("typed_parameter") ||
+                    str.contains("typed_default_parameter ")) &&
+                   line_num == func_definition_line && spaces_count == parameters_spaces_count) {
             func_param_counter++;
         }
     });
